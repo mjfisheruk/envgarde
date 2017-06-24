74d52c6b0cf7103ffad280c3705172f91fcffe1f
Matt Fisher <general@mjfisher.net>
2017-06-24 11:10:35 +0100

    Fmt'd

--- envgarde.go
+++ envgarde.go
@@ -1,48 +1,55 @@
-package main;
+package main
 
-import(
-	"fmt"
-	"os"
+import (
 	"bufio"
-	"log"
 	"flag"
+	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
 
 	var docMode = flag.Bool("d", false, "Run in documentation mode")
 
-	flag.Parse();
+	flag.Parse()
 
 	file, err := os.Open(".envgarde")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer file.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-    scanner := bufio.NewScanner(file)
-    errorCount := 0
-    
-    for scanner.Scan() {
-    	envVarName := scanner.Text()
-    	_, isSet := os.LookupEnv(envVarName)
-    	status := "(OK)"
-    	if !isSet {
-    		status = "(ERROR: Not set)"
-    		errorCount++
-    	}
-        fmt.Println(envVarName, status)
-    }
+	scanner := bufio.NewScanner(file)
+	errorCount := 0
 
-    if err := scanner.Err(); err != nil {
-        log.Fatal(err)
-    }
+	for scanner.Scan() {
+		envVarName := scanner.Text()
+		if *docMode {
+			fmt.Println(envVarName, "(Required)")
+		} else {
+			_, isSet := os.LookupEnv(envVarName)
+			status := "(OK)"
+			if !isSet {
+				status = "(ERROR: Not set)"
+				errorCount++
+			}
+			fmt.Println(envVarName, status)
+		}
+	}
 
-    if errorCount == 0 {
-    	fmt.Println("All envrionment variables set. OK.")
-    	os.Exit(0)
-    } else {
-    	fmt.Printf("Error: %v envrionment variables were not set.\n", errorCount)
-    	os.Exit(1)
-    }
+	if !*docMode {
+		if errorCount == 0 {
+			fmt.Println("All envrionment variables set. OK.")
+			os.Exit(0)
+		} else {
+			fmt.Printf("Error: %v envrionment variables were not set.\n", errorCount)
+			os.Exit(1)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
 }
