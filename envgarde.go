803de9b7bedb68ee4168ff47a6980f22f34c2ce0
Matt Fisher <general@mjfisher.net>
2020-01-27 14:36:16 +0000

    Updates naming for clarity

--- envgarde.go
+++ envgarde.go
@@ -9,71 +9,89 @@ import (
 	"strings"
 )
 
-type envVarRule struct {
+type Rule struct {
 	Name        string
 	Description string
 }
 
-func loadTextFile() ([]envVarRule, error) {
+func loadRules() ([]Rule, error) {
+	config, err := loadRulesFromTextFile()
+	if err != nil {
+		config, err = loadRulesFromYamlFile()
+	}
+	return config, err
+}
+
+func loadRulesFromTextFile() ([]Rule, error) {
 	content, err := ioutil.ReadFile(".envgarde")
 	if err != nil {
 		return nil, err
 	}
+
 	lines := strings.Split(string(content), "\n")
-	config := make([]envVarRule, len(lines))
+	config := make([]Rule, len(lines))
 	for i, line := range lines {
-		config[i] = envVarRule{Name: line, Description: ""}
+		config[i] = Rule{Name: line, Description: ""}
 	}
+
 	return config, nil
 }
 
-func loadYamlFile() ([]envVarRule, error) {
+func loadRulesFromYamlFile() ([]Rule, error) {
 	yamlFile, err := ioutil.ReadFile(".envgarde.yaml")
 	if err != nil {
 		return nil, err
 	}
 
-	var config []envVarRule
+	var config []Rule
 	err = yaml.Unmarshal(yamlFile, &config)
 	return config, err
 }
 
-func main() {
+func printRulesDescription(rules []Rule) {
+	for _, Rule := range rules {
+		fmt.Println(Rule.Name, Rule.Description, "(Required)")
+	}
+}
 
-	var docMode = flag.Bool("d", false, "Run in documentation mode")
+func checkEnvironmentVariables(rules []Rule) {
+	var passedRules = make([]Rule, 0)
+	var failedRules = make([]Rule, 0)
 
-	flag.Parse()
-	config, err := loadTextFile()
-	if err != nil {
-		config, err = loadYamlFile()
-		if err != nil {
-			fmt.Println(err, "No configuration file found.")
-			os.Exit(1)
+	for _, Rule := range rules {
+		_, isSet := os.LookupEnv(Rule.Name)
+		status := "(OK)"
+		if !isSet {
+			failedRules = append(failedRules, Rule)
+			status = "(ERROR: Not set)"
+		} else {
+			passedRules = append(passedRules, Rule)
 		}
+		fmt.Println(Rule.Name, status)
 	}
 
-	if *docMode {
-		for _, envVarRule := range config {
-			fmt.Println(envVarRule.Name, "(Required)")
-		}
+	if len(failedRules) == 0 {
+		fmt.Println("All environment variables set. OK.")
+		os.Exit(0)
 	} else {
-		var errorCount = 0
-		for _, envVarRule := range config {
-			_, isSet := os.LookupEnv(envVarRule.Name)
-			status := "(OK)"
-			if !isSet {
-				status = "(ERROR: Not set)"
-				errorCount++
-			}
-			fmt.Println(envVarRule.Name, status)
-		}
+		fmt.Printf("Error: %v envrionment variables were not set.\n", len(failedRules))
+		os.Exit(1)
+	}
+}
 
-		if errorCount == 0 {
-			fmt.Println("All envrionment variables set. OK.")
-			os.Exit(0)
-		} else {
-			fmt.Printf("Error: %v envrionment variables were not set.\n", errorCount)
-			os.Exit(1)
-		}
+func main() {
+	rules, err := loadRules()
+	if err != nil {
+		fmt.Println(err, "No rules file found.")
+		os.Exit(1)
+	}
+
+	var describeRules = flag.Bool("d", false, "Describe rules")
+	flag.Parse()
+
+	if *describeRules {
+		printRulesDescription(rules)
+	} else {
+		checkEnvironmentVariables(rules)
 	}
 }
